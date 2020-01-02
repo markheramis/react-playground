8cad9922fb58158986046f669e8ac2530ef0f794
Mark Heramis <chumheramis@gmail.com>
2020-01-02 18:13:08 +0800

    add response provider
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- config/app.php
+++ config/app.php
@@ -176,6 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\ResponseMacroServiceProvider::class,
+
+        # 3rd party providers
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class
     ],
 
